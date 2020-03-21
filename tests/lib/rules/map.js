51ab1748a6116514dc661a88907920eea93f05d1
Dmitriy Davydov <dvdvdmt@gmail.com>
2020-03-21 14:37:40 +0500

    prepare project with Yoman generator

---
+++ tests/lib/rules/map.js
@@ -0,0 +1,37 @@
+/**
+ * @fileoverview Replaces _.map() to native Array.map() whenever its possible.
+ * @author 
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/map"),
+
+    RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run("map", rule, {
+
+    valid: [
+
+        // give me some code that won't trigger a warning
+    ],
+
+    invalid: [
+        {
+            code: "return _.map(collection, fn);",
+            errors: [{
+                message: "Fill me in.",
+                type: "Me too"
+            }]
+        }
+    ]
+});
