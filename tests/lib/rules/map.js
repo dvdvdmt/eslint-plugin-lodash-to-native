dd58f554b0c7f1307a9ecceb6648eedf48598e98
Dmitriy Davydov <dvdvdmt@gmail.com>
2020-03-22 12:03:46 +0500

    add detection of several potential fixes in simple scenarios
    add eslint recommended rules in .eslintrc
    
    move eslint to dependencies

--- tests/lib/rules/map.js
+++ tests/lib/rules/map.js
@@ -1,37 +1,30 @@
-/**
- * @fileoverview Replaces _.map() to native Array.map() whenever its possible.
- * @author
- */
-"use strict";
+'use strict';
 
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
+const rule = require('../../../lib/rules/map');
+const {RuleTester} = require('eslint');
 
-var rule = require("../../../lib/rules/map"),
+const ruleTester = new RuleTester();
 
-    RuleTester = require("eslint").RuleTester;
+ruleTester.run('map', rule, {
+  valid: [
+    '_.foo([], fn)',
+    '[1,2,3].map(fn)',
+    '_.map({a:1, b:2, c:3}, fn)',
+    'var a = {}; _.map(a,b);',
+  ],
 
-
-//------------------------------------------------------------------------------
-// Tests
-//------------------------------------------------------------------------------
-
-var ruleTester = new RuleTester();
-ruleTester.run("map", rule, {
-
-    valid: [
-
-        // give me some code that won't trigger a warning
-    ],
-
-    invalid: [
-        {
-            code: "_.map([1,2,3], fn)",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
-        }
-    ]
+  invalid: [
+    {
+      code: '_.map(a,b)',
+      errors: [{messageId: 'replaceLodashMap'}],
+    },
+    {
+      code: '_.map([1,2,3], fn)',
+      errors: [{messageId: 'replaceWithoutCollectionTypeChecking'}],
+    },
+    {
+      code: 'var a = [1,2,3]; _.map(a, fn)',
+      errors: [{messageId: 'replaceWithoutCollectionTypeChecking'}],
+    },
+  ],
 });
